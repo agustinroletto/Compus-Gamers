e7b6bb44c9bfc1f7d6687c4780bff5e9d586f86a
agusroletto <51084228+agustinroletto@users.noreply.github.com>
2021-04-13 00:44:29 -0300

    peque;os cambios

--- proyecto/src/components/ItemDetailContainer/ItemDetailContainer.js
+++ proyecto/src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,14 +1,6 @@
 import SimpleModal from "./ItemDetail";
 
 function ItemDetailContainer({ product }) {
-  // const [products, setProducts] = useState([]);
-
-  // useEffect(() => {
-  //   fetch("https://api.github.com/users")
-  //     .then((response) => response.json())
-  //     .then((data) => setProducts(data)); // ASIGNAR AL STATE ORIGINA VACIO, LA DATA QUE RECIBIMOS DE LA PETICION ASYNC
-  // }, []); // LE PASAMOS UN ARRAY VACIO PARA QUE NO LOOPEE DE FORMA INFINITA;
-
   return (
     <div>
       <SimpleModal products={product} />
