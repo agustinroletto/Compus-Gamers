72275bdc7c946b5809ace7e48211b160af251f3d
agusroletto <51084228+agustinroletto@users.noreply.github.com>
2021-04-27 21:36:47 -0300

    nothing new

---
+++ proyecto/src/Firebase.js
@@ -0,0 +1,17 @@
+import firebase from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyAKblSCA7Caj9W8_74uki8uAwEogr1xbJQ",
+  authDomain: "curso-react-c9276.firebaseapp.com",
+  projectId: "curso-react-c9276",
+  storageBucket: "curso-react-c9276.appspot.com",
+  messagingSenderId: "240404607442",
+  appId: "1:240404607442:web:68b7739888aa8283d9980b",
+  measurementId: "G-JGMJ2XBH76",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const fb = firebase.initializeApp(firebaseConfig);
+const db = fb.firestore;
+export default db;
